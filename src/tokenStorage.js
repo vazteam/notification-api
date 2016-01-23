0da481a8272baa05c53ca0afb65b3b6b2cceb48d
osyoyu <osyoyu@osyoyu.com>
2016-01-23 15:05:33 +0900

    Make tokenStorage a class (TokenStorage)

--- src/tokenStorage.js
+++ src/tokenStorage.js
@@ -3,19 +3,24 @@
 var redis = require('redis');
 var bluebird = require('bluebird');
 
-var exports = module.exports = {};
-
-var client = redis.createClient();
-
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-exports.registerToken = function (id, token) {
-  client.lpush(id, token);
-};
+class TokenStorage {
+  constructor () {
+    this.redis = redis.createClient();
+  }
 
-exports.getTokensById = function (id, callback) {
-  client.lrange(id, 0, -1, (err, reply) => {
-    callback(reply);
-  });
-};
+  registerToken (id, token) {
+    this.lpush(id, token);
+
+  }
+
+  getTokensById (id, callback) {
+    client.lrange(id, 0, -1, (err, reply) => {
+      callback(reply);
+    });
+  }
+}
+
+exports.tokenStorage = TokenStorage;
