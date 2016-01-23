bcdfa674aae19e49c9618938c6d3c1e6a2acceaf
osyoyu <osyoyu@osyoyu.com>
2016-01-23 17:46:42 +0900

    Add some sample scripts

---
+++ tests/notify.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const apn = require('apn');
+const Notifier = require('./../src/notifier.js');
+
+var notifier = new Notifier();
+
+var notification = new apn.Notification();
+notification.expiry = 1;
+notification.badge = 1234;
+notification.sound = "ping.aiff";
+notification.alert = "hoge";
+
+notifier.tokenStorage.getTokensById('1', (tokens) => {
+  console.log(tokens);
+});
+notifier.notify([1], notification);
---
+++ tests/register.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var TokenStorage = require('../src/tokenStorage.js');
+var ts = new TokenStorage();
+
+ts.registerToken('1', 'fc749644c85116c134f539cf0254a1680a48bb70ece5fcd88b9cb79cc74fd0ba');
+// ts.registerToken('2', 'ffff47e528dc484dabf4262bd3199cb8d608d0ad486c0214f0a93347799a51d6');
+
+setTimeout(() => {
+  ts.getTokensById('1', (tokens) => {
+    console.log(tokens);
+  });
+
+
+  ts.getTokensById('2', (tokens) => {
+    console.log(tokens);
+  });
+}, 3000);
