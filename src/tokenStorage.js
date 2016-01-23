a13824efec0c3421d1e6b8336812db21f6447652
osyoyu <osyoyu@osyoyu.com>
2016-01-23 18:09:57 +0900

    Remove bluebird

--- src/tokenStorage.js
+++ src/tokenStorage.js
@@ -1,10 +1,6 @@
 "use strict";
 
 const redis = require('redis');
-const bluebird = require('bluebird');
-
-bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
 
 class TokenStorage {
   constructor () {
